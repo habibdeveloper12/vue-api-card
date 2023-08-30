3cbe604b4d912b10cf0cf49d8df5a663b95304f1
habibur rahman <habibur.rahman@elementzerolabs.com>
2023-08-30 12:58:18 +0600

    done

--- src/components/YouTubeButton.vue
+++ src/components/YouTubeButton.vue
@@ -2,14 +2,14 @@
   <a
     :href="href"
     target="_blank"
-    class="px-3 py-2 rounded border-2 text-white border-orange-600 bg-orange-500 hover:bg-orange-600 transition-colors"
+    class="px-3 py-2 rounded border-2 text-white border-blue-600 bg-blue-500 hover:bg-red-600 transition-colors"
   >
-    <slot>YouTube</slot>
+    <slot>Go Link</slot>
   </a>
 </template>
 
 <script setup>
 const props = defineProps({
-  href: String
-})
+  href: String,
+});
 </script>
